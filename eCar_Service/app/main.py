2eafd754d795c3ca39a9506cb9b6569d31b488f3
Adi Ćefo <167250418+adicefo@users.noreply.github.com>
2025-05-14 19:17:59 +0200

    Added db.py neo4j connection
    Added db.py neo4j connection

--- eCar_Service/app/main.py
+++ eCar_Service/app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from app.db import get_db
+from neo4j import Session
 
 app=FastAPI()
 
 @app.get("/")
 def read_root():
-    return{"message","Welcome to eCar FastAPI Service!"}
+    return{"message":"Welcome to eCar FastAPI Service!"}
+
+@app.get("/test-neo4j")
+def test_neo4j(session: Session = Depends(get_db)):
+    result = session.run("RETURN 'Neo4j is connected!' AS message")
+    message = result.single()["message"]
+    return {"message": message}
