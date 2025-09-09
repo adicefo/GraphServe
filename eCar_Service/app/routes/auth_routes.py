cfb01b1886be3d8098d7691781fc1e8eab91ad6b
Adi Ćefo <167250418+adicefo@users.noreply.github.com>
2025-09-09 22:54:00 +0200

    Added login endpoint

---
+++ eCar_Service/app/routes/auth_routes.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, Depends
+from app.security import authenticate
+from app.models.responses import UserDTO
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+@router.post("/login")
+def login(user=Depends(authenticate)):
+     return {"username": user.username, "uid": user.uid, "email": user.email}
