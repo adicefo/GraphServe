028ead4955f9b7f58fe5c8efa12c7d4d5580c7e0
Adi Ćefo <167250418+adicefo@users.noreply.github.com>
2025-09-16 22:49:49 +0200

    Added logout button
    Added logout button

--- ecar-frontend/src/app/app.routes.ts
+++ ecar-frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DriverComponent } from './pages/driver/driver.component';
+import { ClientComponent } from './pages/client/client.component';
+import { RouteComponent } from './pages/route/route.component';
 
 export const routes: Routes = [
     {path:'',component:LoginComponent},
-    {path:'dashboard',component:DashboardComponent}
+    {path:'dashboard',component:DashboardComponent},
+    {path:'drivers',component:DriverComponent},
+    {path:'clients',component:ClientComponent},
+    {path:'routes',component:RouteComponent},
 ];
--- ecar-frontend/src/app/pages/master/master.component.ts
+++ ecar-frontend/src/app/pages/master/master.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-master',
@@ -14,6 +15,8 @@ export class MasterComponent {
   currentDateTime: string ="";
 
   showInfo: boolean = false;
+
+  constructor(private router: Router) {}
   ngOnInit() {
     this.username = localStorage.getItem('username') || 'User';
 
@@ -31,5 +34,11 @@ export class MasterComponent {
 
   toggleInfo() {
     this.showInfo = !this.showInfo;
+  }
+
+  logout() {
+    localStorage.removeItem('username');
+    localStorage.removeItem('password');
+    this.router.navigate(['']);
   }
 }
