93a2b115ca97bd14caaaa75f405c514b56e619dc
Adi Ćefo <167250418+adicefo@users.noreply.github.com>
2025-09-16 22:44:01 +0200

    Added route component
    Added route component

---
+++ ecar-frontend/src/app/pages/route/route.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MasterComponent } from '../master/master.component';
+
+@Component({
+  selector: 'app-route',
+  imports: [MasterComponent],
+  templateUrl: './route.component.html',
+  styleUrl: './route.component.css'
+})
+export class RouteComponent {
+
+}
