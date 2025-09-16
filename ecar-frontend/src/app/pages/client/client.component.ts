ce2c268ee296fb14c134738d4a8c506506b41e28
Adi Ćefo <167250418+adicefo@users.noreply.github.com>
2025-09-16 22:43:49 +0200

    Added client component
    Added client component

---
+++ ecar-frontend/src/app/pages/client/client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MasterComponent } from '../master/master.component';
+
+@Component({
+  selector: 'app-client',
+  imports: [MasterComponent],
+  templateUrl: './client.component.html',
+  styleUrl: './client.component.css'
+})
+export class ClientComponent {
+
+}
