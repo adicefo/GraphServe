0d19cb733c6d822263f89459ecc65f4e43cb10cf
Adi Ćefo <167250418+adicefo@users.noreply.github.com>
2025-09-16 22:43:33 +0200

    Added driver component
    Added driver component

---
+++ ecar-frontend/src/app/pages/driver/driver.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MasterComponent } from '../master/master.component';
+
+@Component({
+  selector: 'app-driver',
+  imports: [MasterComponent],
+  templateUrl: './driver.component.html',
+  styleUrl: './driver.component.css'
+})
+export class DriverComponent {
+
+}
